86d5d7c2e0a7af9558f3fc426e1b35f55ee0ca76
Kiss György <kissgyorgy@me.com>
2019-09-05 19:23:01 +0200

    Added CloudFlare strace service
    It's the fastest I could find, but it's response must be parsed differently, so
    I wrote tests for it and added pytest as a dependency.

--- cfdns.py
+++ cfdns.py
@@ -2,9 +2,32 @@
 import pickle
 import ipaddress
 from pathlib import Path
+from collections import namedtuple
 import click
 import requests
 import CloudFlare
+
+
+def parse_cloudflare_trace_ip(res: str):
+    """Parses the IP address line from the cloudflare trace service response.
+    Example response:
+        fl=114f30
+        h=1.1.1.1
+        ip=188.6.90.5
+        ts=1567700692.298
+        visit_scheme=https
+        uag=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36
+        colo=VIE
+        http=http/2
+        loc=HU
+        tls=TLSv1.3
+        sni=off
+        warp=off
+    """
+    for line in res.splitlines():
+        if line.startswith("ip="):
+            ip = line[len("ip=") :]
+            return ip
 
 
 def strip_whitespace(res):
@@ -16,6 +39,11 @@ IPService = namedtuple("IPService", "name, url, response_parser")
 
 IP_SERVICES = [
     # fmt: off
+    IPService(
+        "CloudFlare trace",
+        "https://1.1.1.1/cdn-cgi/trace",
+        parse_cloudflare_trace_ip,
+    ),
     IPService(
         "ifconfig.co (https://github.com/mpolden/echoip)",
         "https://ifconfig.co/ip",
---
+++ test_cfdns.py
@@ -0,0 +1,20 @@
+import pytest
+import requests
+import cfdns
+
+
+def test_parse_cloudflare_trace_ip():
+    trace_service_response = """fl=114f30
+h=1.1.1.1
+ip=199.12.81.4
+ts=1567700692.298
+visit_scheme=https
+uag=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36
+colo=VIE
+http=http/2
+loc=HU
+tls=TLSv1.3
+sni=off
+warp=off
+"""
+    assert cfdns.parse_cloudflare_trace_ip(trace_service_response) == "199.12.81.4"
