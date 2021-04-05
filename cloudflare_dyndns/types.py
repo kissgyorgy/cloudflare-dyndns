a28b49de7c4843d6539f87586b748edf4c0b7a23
Kiss György <kissgyorgy@me.com>
2021-04-05 12:40:38 +0200

    New Cache class using Pydantic
    Capable of caching IPv4 and IPv6 addresses, stores the cache object
    in a JSON file.

--- cloudflare_dyndns/types.py
+++ cloudflare_dyndns/types.py
@@ -1,11 +1,12 @@
 import ipaddress
-from typing import Literal, Union
+from typing import Literal, Union, NewType
 
 
 IPv4or6Address = Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
 A = Literal["A"]
 AAAA = Literal["AAAA"]
 RecordType = Union[A, AAAA]
+Domain = NewType("Domain", str)
 
 
 def get_record_type(ip: IPv4or6Address) -> RecordType:
